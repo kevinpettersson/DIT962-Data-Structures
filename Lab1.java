f1af6293cb5b3aca81d235de35c3b99bb2708642
kevinpettersson <kevin.petterssons@hotmail.com>
2024-04-02 22:38:33 +0200

    forsätt

--- Lab1.java
+++ Lab1.java
@@ -5,14 +5,21 @@ import java.util.ArrayList.*;
 public class Lab1 {
     /** Sorting algorithms **/
 
-    public static void main(String[] args){
+   public static void main(String[] args){
         int[] tmp = {2,1,5,17,3,7,4,99,323,5,214};
         insertionSort(tmp);
         System.out.println(Arrays.toString(tmp));
 
         int[] tmp2 = {2,1,5,17,3,7,4,99,323,5,214};
+        int[] tmp4 = {0,2};
+        quickSort(tmp4, 0, tmp4.length-1);
+        System.out.println(Arrays.toString(tmp4));
         quickSort(tmp2, 0, tmp2.length -1);
         System.out.println(Arrays.toString(tmp2));
+
+        int[] tmp3 = {2,1,5,17,3,7,4,99,323,5,214};
+        mergeSort(tmp3);
+        System.out.println(Arrays.toString(tmp3));
     }
 
     // Insertion sort.
@@ -35,8 +42,8 @@ public class Lab1 {
 
     // Quick sort.
 
-    private static void quickSort(int[] array, int begin, int end){
-
+    public static void quickSort(int[] array, int begin, int end){
+        
         if(begin >= end){                          //When the array is sorted the subarray will only have 1 element and we imeditely return the method. 
 
             return;
@@ -52,38 +59,84 @@ public class Lab1 {
         int pointerLeft = begin+1;
         int pointerRight = end;
 
-        while(pointerLeft < pointerRight){      //Will increment/decrement the pointers as long as the pointers pointing at "sorted" elements.
+        while(pointerLeft <= pointerRight){      //Will increment/decrement the pointers as long as the pointers pointing at "sorted" elements.
 
-            while(array[pointerLeft] < pivot && pointerLeft < pointerRight){ pointerLeft++; }
+            while(array[pointerLeft] < pivot && pointerLeft < pointerRight) { pointerLeft++; }
     
             while(array[pointerRight] > pivot && pointerLeft < pointerRight){ pointerRight--; }
 
-            swap(array, pointerLeft, pointerRight);
-            pointerLeft++;
-            pointerRight--;
+            if(pointerLeft != pointerRight){
+                swap(array, pointerLeft, pointerRight);
+                pointerLeft++;
+                pointerRight--;
+            }
+            
+            if(pointerLeft == pointerRight){
+                pointerLeft++;
+                pointerRight--;
+            }
         }
-
-        if(pointerLeft >= pointerRight){        //When the left pointer is larger than or equal to the right pointer
-            swap(array, pointerRight, begin);   //we know that the pivot should be placed in that index.
-        }
-
+                                                //When the left pointer is larger than or equal to the right pointer
+        swap(array, pointerRight, begin);       //we know that the pivot should be placed in that index.
+        
         return pointerRight;                    //Return the index of where the pivot element ended up.
     }
 
     // Mergesort.
 
-    public static int[] mergeSort(int[] array) {
-        throw new UnsupportedOperationException();
+    public static void mergeSort(int[] array) {
+        int length = array.length;
+
+        if(length < 2){       
+            return;
+        }
+
+        int mid = length / 2;
+        int[] leftArr  = new int[mid];
+        int[] rightArr = new int[length - mid]; //(length - mid) For correct splittning of the subarrays taking care of uneven array lengths.
+
+        for (int i = 0; i < mid; i++) {         //Copying elements for each side of the split.
+            leftArr[i] = array[i];
+        }
+        for (int i = mid; i < length; i++) {
+            rightArr[i - mid] = array[i];       //(i - mid) Setting the element at the correct indices and not what mid represent.
+        }
+
+        mergeSort(leftArr);                     //Recursive calls for each splitted array.
+        mergeSort(rightArr);
+        merge(array, leftArr, rightArr);
     }
 
     // Mergesort part of an array
-    private static int[] mergeSort(int[] array, int begin, int end) {
-        throw new UnsupportedOperationException();
-    }
+    private static void merge(int[] array, int[] leftArr, int[] rightArr) {
+        int left = leftArr.length;
+        int right = rightArr.length;
 
-    // Merge two sorted arrays into one
-    private static int[] merge(int[] left, int[] right) {
-        throw new UnsupportedOperationException();
+        int i = 0, j = 0, k = 0;            //The pointers for each array, our left, right and merged array.
+                                            //k = merged, i = left, j = right.
+
+        while(i < left && j < right){      
+
+            if(leftArr[i] <= rightArr[j]){
+                array[k] = leftArr[i];
+                i++;
+            } else {
+                array[k] = rightArr[j];
+                j++;
+            }
+            k++;
+        }
+
+        while(i < left){                   // In case the while loop misses an index we directly copy it to the sorted array in correct index.
+            array[k] = leftArr[i];
+            i++;
+            k++;
+        }
+        while(j < right){
+            array[k] = rightArr[j];
+            j++;
+            k++;
+        }
     }
 
     // Swap two elements in an array

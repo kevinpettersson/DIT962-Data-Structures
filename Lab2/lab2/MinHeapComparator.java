e83bafdd3339f5de0a637fc7dca6d4b0f8e81347
Kevin <egokevin@hotmail.com>
2024-04-22 20:04:10 +0200

    är less

---
+++ Lab2/lab2/MinHeapComparator.java
@@ -0,0 +1,12 @@
+package lab2;
+import java.util.Comparator;
+
+public class MinHeapComparator implements Comparator<Bid> {
+
+    @Override
+    public int compare(Bid oldBid, Bid newBid) {
+        
+        return Integer.compare(newBid.getPrice(), oldBid.getPrice());
+    }
+    
+}
